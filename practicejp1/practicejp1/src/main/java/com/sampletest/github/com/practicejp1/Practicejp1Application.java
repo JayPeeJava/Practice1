4534149872f1ac8193eb375e80f825f07ca79d42
JayPee24 <jp@lapapa88.com>
2023-07-27 11:19:20 +0800

    Initialize commit All

---
+++ practicejp1/practicejp1/src/main/java/com/sampletest/github/com/practicejp1/Practicejp1Application.java
@@ -0,0 +1,13 @@
+package com.sampletest.github.com.practicejp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practicejp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practicejp1Application.class, args);
+	}
+
+}
